03769469b02183e3da7409548c5fdbf897d4875b
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-09-22 14:45:17 -0400

    Added the column specs to silence the warning.

--- server.R
+++ server.R
@@ -11,8 +11,22 @@ library(readr)
 library(tidyr)
 library(glue)
 
-aa_map <- read_csv("src/amino_acid_codon_map.csv")
-g_scores <- read_csv("src/grantham_scores.csv")
+aa_col_spec <- cols(
+  SINGLE = col_character(),
+  NAME = col_character(),
+  TRIPLET = col_character(),
+  CODON = col_character()
+)
+
+g_col_spec <- cols(
+  FIRST = col_character(),
+  SECOND = col_character(),
+  SCORE = col_double()
+)
+
+aa_map <- read_csv("src/amino_acid_codon_map.csv", col_types = aa_col_spec)
+g_scores <- read_csv("src/grantham_scores.csv", col_types = g_col_spec)
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
--- ui.R
+++ ui.R
@@ -9,8 +9,22 @@ library(shiny)
 library(readr)
 library(tidyr)
 
-aa_map <- read_csv("src/amino_acid_codon_map.csv")
-g_scores <- read_csv("src/grantham_scores.csv")
+
+aa_col_spec <- cols(
+  SINGLE = col_character(),
+  NAME = col_character(),
+  TRIPLET = col_character(),
+  CODON = col_character()
+)
+
+g_col_spec <- cols(
+  FIRST = col_character(),
+  SECOND = col_character(),
+  SCORE = col_double()
+)
+
+aa_map <- read_csv("src/amino_acid_codon_map.csv", col_types = aa_col_spec)
+g_scores <- read_csv("src/grantham_scores.csv", col_types = g_col_spec)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
